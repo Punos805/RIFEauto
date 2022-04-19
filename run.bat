3fcd13c5d21781211fed9781da3414a885365af9
Punos805 <93321730+Punos805@users.noreply.github.com>
2022-04-20 08:24:01 +0900

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,9 @@
+mkdir input_frames
+mkdir output_frames
+
+ffprobe input.mp4
+ffmpeg -i input.mp4 -vn -acodec copy audio.m4a
+ffmpeg -i input.mp4 input_frames/frame_%08d.png
+
+./rife-ncnn-vulkan -i input_frames -o output_frames
+ffmpeg -framerate 48 -i output_frames/%08d.png -i audio.m4a -c:a copy -crf 20 -c:v libx264 -pix_fmt yuv420p output.mp4
