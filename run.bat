4ad3169ceff5a6834b0b200e4ee915e20fd3bf3e
Punos805 <93321730+Punos805@users.noreply.github.com>
2022-04-20 17:25:05 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,3 +7,5 @@ ffmpeg -i input.mp4 input_frames/frame_%08d.png
 
 ./rife-ncnn-vulkan -i input_frames -o output_frames
 ffmpeg -framerate 48 -i output_frames/%08d.png -i audio.m4a -c:a copy -crf 20 -c:v libx264 -pix_fmt yuv420p output.mp4
+
+:::This is 100% bat script, so source code is in the release file.
